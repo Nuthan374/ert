185da4aa634ff0be4f5fcbe6b783ac57f770b3e4
Anonymous User <nobody@example.com>
2024-05-21 12:44:00 +0000

    commit

---
+++ loan-risk-analysis/src/main/java/com/example/creditrisk/CreditRiskBatchApplication.java
@@ -0,0 +1,11 @@
+package com.example.creditrisk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditRiskBatchApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CreditRiskBatchApplication.class, args);
+    }
+}
